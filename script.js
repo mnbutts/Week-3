d5c67797ce76fd09321bb5abb4f3e1f482f4e5c8
mnbutts <michellebutts@Michelles-MacBook-Pro.local>
2016-09-18 19:27:09 -0400

    added console.log
    added console.log

--- script.js
+++ script.js
@@ -5,5 +5,5 @@
   are used to respond to user interaction with the "Donate" button.
 */
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Donate" button.
+  console.log("Hello World!");
 });
